5fa0e38775a3eaa344329a8b35bc0a832b1b1db4
wijihye <wijihye@naver.com>
2020-02-25 19:29:50 +0900

    2164(카드 2)

---
+++ 2164(카드2).cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <utility>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    queue<int> q;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        q.push(i);
+    }
+
+    while (q.size() != 1)
+    {
+        // 탈출 조건 이전에 pop -> 런타임에러.
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << q.front();
+
+    return 0;
+}
