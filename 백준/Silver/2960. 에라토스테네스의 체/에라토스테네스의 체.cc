dc870b2ee25e9bcc8f4041bb35ddc865fe908925
Jihye Jang <58647487+wijihye@users.noreply.github.com>
2022-07-07 16:29:34 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+int GCD(int a, int b) {
+	if (b == 0) return a;
+	return GCD(b, a % b);
+	// 꼬리재귀, 컴파일러에서 시간 좀 줄여주기에 바로 함수 자체를 리턴 하는 것이 효율적임.
+}
+
+int arr[1001];
+
+
+int main() {
+
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	int ans = 0;
+
+	for (int i = 2; i <= n; i++) {
+		if (arr[i] == 1) continue; // 이미 지워진 곳은 갈 필요 없음
+
+		for (int j = i; j <= n; j += i) {
+			if (arr[j] == 1) continue; // 이미 체크 된 곳이면 세면 안됨 (k감소 금지)
+			
+			arr[j] = 1; // 배수 체크
+			k--; // 카운트
+			if (k == 0) ans = j; // 답 체크
+		}
+
+		if (k == 0) break;
+	}
+
+	printf("%d", ans);
+	
+	return 0;
+}
