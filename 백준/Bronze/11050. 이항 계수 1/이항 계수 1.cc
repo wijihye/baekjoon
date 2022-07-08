ce12406bd627c854958bbd62c60d3f1318ee662b
Jihye Jang <58647487+wijihye@users.noreply.github.com>
2022-07-08 14:06:21 +0900

    구현 방법 추가
    파스칼의 삼각형으로 구현하는 방법 추가

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -22,3 +22,32 @@ int main() {
 
 	return 0;
 }
+
+// 파스칼의 삼각형 이용
+
+// #include <iostream>
+// #include <stdio.h>
+// #include <algorithm>
+// #include <math.h>
+
+// using namespace std;
+
+// int pascal_triangle[12][12];
+
+// int main() {
+
+// 	int n, k;
+// 	scanf("%d %d", &n, &k);
+
+
+// 	for (int i = 0; i <= 11; i++) {
+// 		for (int j = 0; j <= i; j++) {
+// 			if (i == 0) pascal_triangle[i][j] = 1;
+// 			else pascal_triangle[i][j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j];
+// 		}
+// 	}
+
+// 	printf("%d", pascal_triangle[n][k]);
+
+// 	return 0;
+// }
