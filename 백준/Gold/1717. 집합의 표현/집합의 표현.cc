cd9d0409b8f44e963f96d23d6ba9ac3a1af7ee89
Jihye Jang <58647487+wijihye@users.noreply.github.com>
2022-07-11 20:03:10 +0900

    Update 집합의 표현.cc

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,48 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
-#include <vector> // 인접 리스트: 정점 별로 크기가 다르므로 동적 배열(리스트)로 작성 예정
-#include <deque> // In-degree가 0이 되는 정점을 push, pop 하는 자료구조
 
 using namespace std;
-
-//int N, M, a, b;
-//
-//vector<int> AL[32001]; // 인접 리스트, 학생 수가 최대 32000명
-//int InDegree[32001]; // 해당 정점에 대한 In-degree 차수 저장
-//
-//int Answer[32001], idx; // 개인별 스타일에 따라 사용 할지 안 할지 판단
-//
-//int main() {
-//	scanf("%d %d", &N, &M);
-//	for (int i = 0; i < M; i++) {
-//		scanf("%d %d", &a, &b);
-//		AL[a].push_back(b); // 방향성 있는 간선이므로 한 곳만 추가
-//		++InDegree[b]; // a->b 이므로 b의 In-Degree 증가
-//	}
-//
-//	deque<int> q; // In Degree 0되는 정점 저장
-//	for (int i = 1; i <= N; i++) {
-//		if (!InDegree[i])q.push_back(i);
-//	}
-//
-//	while (!q.empty()) {
-//		int curr = q.front(); // 현재 맨 앞에 있는 정점
-//		q.pop_front();
-//		Answer[idx++] = curr;
-//		for (int next : AL[curr]) {
-//			if (!--InDegree[next]) q.push_back(next);
-//		}
-//	}
-//
-//	printf("%d", Answer[0]);
-//	for (int i = 1; i < idx; i++) {
-//		printf("%d ", Answer[i]);
-//	}
-//
-//	return 0;
-//}
-
 
 int N, M, a, b, c;
 
